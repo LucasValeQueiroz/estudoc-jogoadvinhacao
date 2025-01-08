a42b2971623ce783ce0ac1179ce9014c79131565
Lucas Vale <lucasvale0904@gmail.com>
2025-01-08 09:57:43 -0300

    V 0.6

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -8,16 +8,17 @@ int main () {
     cout << "* Bem-vindos ao jogo da adivinhacao! *" << endl;
     cout << "**************************************" << endl;
     
-    
+    //Constante para o copilador indentificar que nunca vai mudar o valor no codigo
     const int NUMERO_SECRETO = 42;
-    //guardando as condicionais com o bool (guarda 1 caso a condição é verdadeira)(true)
+    
+    //guardando as condicionais com o bool (continua o loop caso a condição é verdadeira)(true)
     bool nao_acertou = true;
     int tentativas = 0;
 
     //PONTUACAO
     double pontos = 1000.0 ;
     double diferenca;   
-
+    
     while(nao_acertou){
         tentativas++;
         int chute;
@@ -33,9 +34,20 @@ int main () {
         bool acertou = chute == NUMERO_SECRETO;
         bool maior = chute > NUMERO_SECRETO;
         if(acertou){
+            int aux;
             cout << "Parabéns, você acertou!" << endl;
-            cout << "Você fez " << pontos << " pontos" << endl;
-            nao_acertou = false;
+            cout.precision(2);
+            cout << fixed;
+            cout << "Você fez " << pontos << " pontos" << endl << "***********************"<< endl << "*QUER JOGAR NOVAMENTE?*"<< endl << "***********************" << endl << endl;
+            cout << "1 para SIM | 2 para NAO : ";
+            cin >> aux;                                
+            if(aux == 2)nao_acertou = false; //Finaliza o loop
+            else if (aux == 1){
+                cout << endl << "REINICIANDO O JOGO" << endl;
+                tentativas = 0;
+                pontos = 1000.0;
+            }
+    
         }else if (maior){
             diferenca = ((chute - NUMERO_SECRETO))/2.0;
             pontos = pontos - diferenca;
